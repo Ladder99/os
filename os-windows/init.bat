676307e1b08930bf678be8577be00a794ee76724
Brian Burns <bburns.km@gmail.com>
2022-02-10 23:21:31 -0600

    wip init

--- os-windows/init.bat
+++ os-windows/init.bat
@@ -5,13 +5,26 @@
 ::   CD evaluates to C:\Windows\System32.
 ::   USERPROFILE evaluates to C:\Users\ODYSSEY (at least for my winbox).
 
+:: adapter uses L99* vars to know where to read data from.
+
 :: set variables local to this file
 set SETUP=client-lockheed
 set LADDER99=%SYSTEMDRIVE%\ladder99
+set L99_SETUP_FOLDER=%LADDER99%\%SETUP%
+set L99_MODULES_FOLDER=%LADDER99%\ladder99-ce\modules
 
-:: now write them to registry also
+:: set normal user registry
 setx SETUP %SETUP%
 setx LADDER99 %LADDER99%
+setx L99_SETUP_FOLDER %L99_SETUP_FOLDER%
+setx L99_MODULES_FOLDER %L99_MODULES_FOLDER%
+
+:: set machine-wide registry
+:: this way, adapter window service can read them
+setx /m SETUP %SETUP%
+setx /m LADDER99 %LADDER99%
+setx /m L99_SETUP_FOLDER %L99_SETUP_FOLDER%
+setx /m L99_MODULES_FOLDER %L99_MODULES_FOLDER%
 
 :: service name
 setx SERVICE "Ladder99 Adapter"
@@ -21,22 +34,6 @@ setx NODE %LADDER99%\bin\node-v16.13.2-win-x64\node.exe
 setx AGENT %LADDER99%\bin\cppagent-1.8.0.2-win64\bin\agent.exe
 setx NSSM %LADDER99%\bin\nssm-2.24\win64\nssm.exe
 setx GIT %LADDER99%\bin\PortableGit\cmd\git.exe
-
-:: adapter uses these to know where to read data from.
-:: /m writes to the system (machine) environment variables.
-:: that way, adapter window service can read them.
-::setx /m L99_SETUP_FOLDER %LADDER99%\%SETUP%
-::setx /m L99_MODULES_FOLDER %LADDER99%\ladder99-ce\modules
-:: yeah this is crazy
-:: set vars local to this file
-set L99_SETUP_FOLDER=%LADDER99%\%SETUP%
-set L99_MODULES_FOLDER=%LADDER99%\ladder99-ce\modules
-:: set normal user registry
-setx L99_SETUP_FOLDER %L99_SETUP_FOLDER%
-setx L99_MODULES_FOLDER %L99_MODULES_FOLDER%
-:: set machine-wide registry
-setx /m L99_SETUP_FOLDER %L99_SETUP_FOLDER%
-setx /m L99_MODULES_FOLDER %L99_MODULES_FOLDER%
 
 @echo.
 @echo Make sure you run this as administrator, and that the last
