f94d64b039a71df57a1d4f53e5a75829b45dd81e
Brian Burns <bburns.km@gmail.com>
2022-02-17 04:19:44 -0600

    cmts

--- windows/update-code.bat
+++ windows/update-code.bat
@@ -1,5 +1,12 @@
 :: update git repos from github
 
+:: note: the first time git is run from PortableGit, it will ask which
+:: credential manager to use - use the default selection of manager-core. 
+:: this will use the version of github credential manager core that 
+:: comes with PortableGit.
+:: it allows you to pull from a private repo without having to re-enter
+:: your username and passcode every time.
+
 cd %LADDER99%\ladder99-ce && %GIT% pull && %GIT% checkout develop
 cd %LADDER99%\%SETUP% && %GIT% pull
 cd %LADDER99%\os && %GIT% pull
